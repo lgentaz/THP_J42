8952c1372be3d9ca76eeffb856d35cde1787ce3f
lgentaz <lucile.gentaz@gmail.com>
2020-05-28 11:54:07 +0200

    routee

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  root to: "email#index"
+  
   resources :email
   resources :tasks, except: [:show]
+
+  root 'email#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
