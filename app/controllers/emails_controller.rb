58b258eaa0e4b98eb775088f00431f1c3e43c48b
lgentaz <lucile.gentaz@gmail.com>
2020-05-28 14:11:03 +0200

    back to root

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,4 +1,22 @@
 class EmailsController < ApplicationController
   def index
   end
+
+  def create
+    @email = Email.new(object: Faker::Lorem.sentence, body: Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 4))
+    if @email.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js {}
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js {}
+      end
+    end
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  
+  get 'email/index'
   devise_for :users
-  
-  resources :emails
+  root to: "home#index"
+  get 'email/index'
   resources :tasks, except: [:show]
-
-  root 'emails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
